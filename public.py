2372d80cddac670bb725cafdf33f4bdac5de4688
Arjun R Warrier <notwarrier@gmail.com>
2022-11-04 10:38:42 +0530

    Added %s model to insert data

--- public.py
+++ public.py
@@ -15,9 +15,13 @@ data_info = json.loads(data)
 
 for i in data_info['entries']:
 
-    https = str(i['HTTPS'])
-    sql = "INSERT INTO `publicapidata`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ('"+i['API']+"','"+i['Description']+"','"+i['Auth']+"','"+https+"','"+i['Cors']+"','"+i['Link']+"','"+i['Category']+"')"
-    #sql = 'INSERT INTO `publicapidata`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ("'+i["API"]+'","'+i["Description"]+'","'+i["Auth"]+'","'+https+'","'+i["Cors"]+'","'+i["Link"]+'","'+i["Category"]+'")'
-    mycursor.execute(sql)
-    mydb.commit()
+   # https = str(i['HTTPS'])
+    try:
+        sql = "INSERT INTO `publicapidata`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        #sql = 'INSERT INTO `publicapidata`(`api`, `description`, `auth`, `https`, `cors`, `link`, `category`) VALUES ("'+i["API"]+'","'+i["Description"]+'","'+i["Auth"]+'","'+https+'","'+i["Cors"]+'","'+i["Link"]+'","'+i["Category"]+'")'
+        data = (i['API'],i['Description'],i['Auth'],i['HTTPS'],i['Cors'],i['Link'],i['Category'])
+        mycursor.execute(sql,data)
+        mydb.commit()
+    except mysql.connector.Error as e:
+        print("Error ",e)
     print("Data inserted successfully, ", i['API'])
